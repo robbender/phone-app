0fe5976d795034fde669ec90afaee3734abba8c0
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-07-16 13:44:59 -0700

    Updated image schema migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->Increments('id');
             $table->string('name');
-            $table->string('image');
+            $table->mediumText('image')->nullable();
             $table->string('position');
             $table->string('phone');
             // $table->string('size');
