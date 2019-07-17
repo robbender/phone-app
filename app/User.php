e069376a1957a58afdb3d9fb1162da21f9bdace7
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-07-17 12:24:23 -0700

    Created task controller, migration and model

--- app/User.php
+++ app/User.php
@@ -21,21 +21,8 @@ class User extends Authenticatable
 
     protected $guarded = [];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    // protected $hidden = [
-    //     'password', 'remember_token',
-    // ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    // protected $casts = [
-    //     'email_verified_at' => 'datetime',
-    // ];
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
