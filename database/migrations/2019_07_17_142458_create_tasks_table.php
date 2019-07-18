beb0c3e84ac1241bacab9163d6a822b22d4b3868
Robert Bender <robertbender@lynnes-ipad.musd.mpsaz.org>
2019-07-18 07:06:29 -0700

    Updated tasks migration

--- database/migrations/2019_07_17_142458_create_tasks_table.php
+++ database/migrations/2019_07_17_142458_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('project_id');
+            $table->unsignedInteger('user_id');
             $table->string('description');
             $table->boolean('completed')->default(false);
             $table->timestamps();
